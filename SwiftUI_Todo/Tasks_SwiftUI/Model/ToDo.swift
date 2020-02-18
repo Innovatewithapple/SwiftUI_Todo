d8bf50b4218c04e452b06d40db2ccb2b72a0ae19
Mihir Vyas <mihirv126@gmail.com>
2020-02-18 16:41:41 +0530

    Add files via upload

---
+++ SwiftUI_Todo/Tasks_SwiftUI/Model/ToDo.swift
@@ -0,0 +1,30 @@
+//  Tasks_SwiftUI
+//
+//  Created by Mihir Vyas on 17/02/20.
+//  Copyright © 2020 Mihir Vyas. All rights reserved.
+//
+
+
+import SwiftUI
+import Foundation
+import CoreData
+
+// ToDo code generation is turned OFF in the xcdatamodeld file
+public class ToDo: NSManagedObject, Identifiable {
+    @NSManaged public var title: String?
+    @NSManaged public var due: Date
+    @NSManaged public var isNotify: Bool
+}
+
+extension ToDo {
+    
+    static func allToDoFetchRequest() -> NSFetchRequest<ToDo> {
+        let request: NSFetchRequest<ToDo> = ToDo.fetchRequest() as! NSFetchRequest<ToDo>
+        
+        // The @FetchRequest property wrapper in the ContentView requires a sort descriptor
+        request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
+          
+        return request
+    }
+}
+
---
+++ SwiftUI_Todo/Tasks_SwiftUI/View/TaskRow.swift
@@ -0,0 +1,41 @@
+//  Tasks_SwiftUI
+//
+//  Created by Mihir Vyas on 17/02/20.
+//  Copyright © 2020 Mihir Vyas. All rights reserved.
+//
+
+
+import SwiftUI
+import CoreData
+import Combine
+
+struct TaskRow: View {
+    @State var todo: ToDo
+    @State var todayDate: Date
+    
+    var body: some View {
+        VStack(alignment: .trailing, spacing: 10) {
+            HStack {
+                Text(todo.title ?? "Empty")
+                    .bold()
+                    .foregroundColor(.white)
+                    .font(.system(size: 22))
+                    .clipped()
+                    .minimumScaleFactor(0.8)
+                    .lineLimit(nil)
+               
+                Image(systemName: todo.isNotify ? "bell.fill" : "bell.slash.fill")
+                    .padding()
+                    .imageScale(.large)
+                    .aspectRatio(contentMode: .fill)
+                    .foregroundColor(Color.black)
+            }
+            Text(todo.due.isEqual(currentDate: todayDate) ? "Today" : todo.due.string(format: "dd-MM-yyyy"))
+                .bold()
+                .foregroundColor(todo.due.isEqual(currentDate: todayDate) ? Color.blue : Color.primary)
+                .font(.system(size: 19))
+                .frame(height: 20, alignment: .trailing)
+        } .padding()
+    }
+}
+
